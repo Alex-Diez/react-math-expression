dcd613f381ce75af3c1a0826657ada410abc6836
Alex-Diez <alex.dukhno@icloud.com>
2018-03-12 21:53:10 +0200

    implement expression parser

---
+++ src/__test__/model.test.ts
@@ -0,0 +1,101 @@
+import {Parser, Const, Operation} from "../model/parser";
+
+describe('Parser', () => {
+  it('parses negative number', () => {
+    expect(new Parser('-12').parse()).toEqual(new Const(-12));
+  });
+
+  it('parses addition', () => {
+    expect(new Parser('4+3').parse()).toEqual(new Operation('+', new Const(4), new Const(3)));
+  });
+
+  it('parses subtraction', () => {
+    expect(new Parser('4-9').parse()).toEqual(new Operation('-', new Const(4), new Const(9)));
+  });
+
+  it('parses multiplication', () => {
+    expect(new Parser('3×6').parse()).toEqual(new Operation('×', new Const(3), new Const(6)));
+  });
+
+  it('parses division', () => {
+    expect(new Parser('16÷4').parse()).toEqual(new Operation('÷', new Const(16), new Const(4)));
+  });
+
+  it('parses many operations', () => {
+    expect(new Parser('4+5×3-15÷3').parse()).toEqual(
+      new Operation(
+        '-',
+        new Operation(
+          '+',
+          new Const(4),
+          new Operation(
+            '×',
+            new Const(5),
+            new Const(3)
+          )
+        ),
+        new Operation(
+          '÷',
+          new Const(15),
+          new Const(3)
+        )
+      )
+    );
+  });
+
+  it('parses expression with parenthesis', () => {
+    expect(new Parser('(4+5)×3-(30-15)÷3').parse()).toEqual(
+      new Operation(
+        '-',
+        new Operation(
+          '×',
+          new Operation(
+            '+',
+            new Const(4),
+            new Const(5)
+          ),
+          new Const(3)
+        ),
+        new Operation(
+          '÷',
+          new Operation(
+            '-',
+            new Const(30),
+            new Const(15)
+          ),
+          new Const(3)
+        )
+      )
+    );
+  });
+
+  it('skips spaces when parsing negative number', () => {
+    expect(new Parser('  -   12').parse()).toEqual(new Const(-12));
+  });
+
+  it('skips white spaces when parsing expression', () => {
+    expect(new Parser('  (  4 + 5 ) × 3 - ( 30 - 15 ) ÷  3  ').parse()).toEqual(
+      new Operation(
+        '-',
+        new Operation(
+          '×',
+          new Operation(
+            '+',
+            new Const(4),
+            new Const(5)
+          ),
+          new Const(3)
+        ),
+        new Operation(
+          '÷',
+          new Operation(
+            '-',
+            new Const(30),
+            new Const(15)
+          ),
+          new Const(3)
+        )
+      )
+    );
+  });
+});
---
+++ src/model/parser.ts
@@ -0,0 +1,113 @@
+type AstNode = Const | Operation;
+
+class Parser {
+
+  private index: number = 0;
+  private readonly numbers: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  private readonly lowOrderOperators: Array<string> = ['+', '-'];
+  private readonly highOrderOperators: Array<string> = ['×', '÷'];
+
+  constructor(private expression: string) {
+  }
+
+  parse(): AstNode {
+    return this.parseExpression();
+  }
+
+  private parseExpression() {
+    let arg = this.parseTerm();
+    let operator;
+    while ((operator = this.parseLowOrderOperator())) {
+      this.skip();
+      arg = new Operation(operator, arg, this.parseTerm());
+    }
+    return arg;
+  }
+
+  private parseLowOrderOperator(): string | undefined {
+    const operator = this.parseOperator();
+    return operator && this.lowOrderOperators.indexOf(operator) !== -1 ? operator : undefined;
+  }
+
+  private parseTerm(): AstNode {
+    let arg = this.parseArgument();
+    let operator;
+    while ((operator = this.parseHighOrderOperator())) {
+      this.skip();
+      arg = new Operation(operator, arg, this.parseArgument());
+    }
+    return arg;
+  }
+
+  private parseHighOrderOperator(): string | undefined {
+    const operator = this.parseOperator();
+    return operator && this.highOrderOperators.indexOf(operator) !== -1 ? operator : undefined;
+  }
+
+  private skip() {
+    this.index++;
+  }
+
+  private parseArgument(): AstNode {
+    this.skipWhitespaces();
+    return this.nextIsSubexpression() ? this.parseSubexpression() : this.parseConst();
+  }
+
+  private parseSubexpression() {
+    this.skip();
+    const node = this.parseExpression();
+    this.skip();
+    return node;
+  }
+
+  private nextIsSubexpression() {
+    return this.expression[this.index] == '(';
+  }
+
+  private parseConst() {
+    const sign = this.resolveSign();
+    this.skipWhitespaces();
+    const start = this.index;
+    let end = this.index;
+    while (end < this.expression.length && this.numbers.indexOf(this.expression[end]) !== -1) {
+      end++;
+    }
+    this.index = end;
+    return new Const(sign * Number(this.expression.slice(start, end)));
+  }
+
+  private resolveSign(): number {
+    if (this.expression[this.index] == '-') {
+      this.skip();
+      return -1;
+    }
+    return 1;
+  }
+
+  private parseOperator(): string | undefined {
+    this.skipWhitespaces();
+    return this.index < this.expression.length ? this.expression[this.index] : undefined;
+  }
+
+  private skipWhitespaces() {
+    while (this.currentCharIsWhitespace()) {
+      this.skip();
+    }
+  }
+
+  private currentCharIsWhitespace() {
+    return this.expression[this.index] == ' ';
+  }
+}
+
+class Const {
+  constructor(private value: number) {
+  }
+}
+
+class Operation {
+  constructor(private operator: string, private left: Const | Operation, private right: Const | Operation) {
+  }
+}
+
+export {Parser, Const, Operation};
